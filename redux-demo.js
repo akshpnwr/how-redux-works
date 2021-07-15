850e46a394a1b12f702c5cc4c73f461962cd57e7
akshpnwr <akashpanwar9976@gmail.com>
2021-07-15 21:07:29 +0530

    dispatching diff actions

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,11 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "increment") return { counter: state.counter + 1 };
+
+  if (action.type === "decrement") return { counter: state.counter - 1 };
+
+  return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -19,4 +21,4 @@ store.subscribe(counterSubscriber);
 
 store.dispatch({ type: "increment" });
 
-store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
